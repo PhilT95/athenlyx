e186c70feaabf3145cf284e7705b6abf6401e875
PhilT95 <github@phil-trommer.com>
2025-07-23 22:51:01 +0200

    troubleshooting script the third?

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,11 +6,11 @@ cd /usr/docs
 echo $(date) :  Starting sync with git... >> scripts/sync.log
 
 
-prev= echo $(git rev-list HEAD -n 1)
-echo $(date) :  Current git HEAD is  $prev>> scripts/sync.log
+prev="$(git rev-list HEAD -n 1)"
+echo $(date) :  Current git HEAD is  $prev >> scripts/sync.log
 git pull --rebase
-now= echo $(git rev-list HEAD -n 1)
-echo $(date) :  New git HEAD after git pull is $now>> scripts/sync.log
+now="$(git rev-list HEAD -n 1)"
+echo $(date) :  New git HEAD after git pull is $now >> scripts/sync.log
 
 if [ "$now" = "$prev" ]; then
 	echo $(date) :  Changes detected. Rebuilding web page... >> scripts/sync.log
