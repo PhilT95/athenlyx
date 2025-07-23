b58a054b1251e03c520744c9eda860009d990537
PhilT95 <github@phil-trommer.com>
2025-07-23 22:37:59 +0200

    updated sync.sh script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,14 +7,15 @@ echo $(date) ":  Starting sync with git..." >> scripts/sync.log
 
 
 prev= echo $(git rev-list HEAD -n 1)
-echo $(date) ":  Current git HEAD is " $prev >> scripts/sync.log
+echo $(date) ":  Current git HEAD is $prev">> scripts/sync.log
 git pull --rebase
 now= echo $(git rev-list HEAD -n 1)
-echo $(date) ":  New git HEAD after git pull is " $now >> scripts/sync.log
+echo $(date) ":  New git HEAD after git pull is $now" >> scripts/sync.log
 
 if [ "$now" = "$prev" ]; then
 	echo $(date) ":  Changes detected. Rebuilding web page..." >> scripts/sync.log
-	mkdocs build
+	cd /usr/docs
+	echo $(mkdocs build) >> scripts/build.log
 	cp /usr/docs/site/* /www/ -rf
 	echo $(date) ":  Sync finalized." >> scripts/sync.log
 fi
